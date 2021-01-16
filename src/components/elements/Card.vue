94a8d77898382b2a063d2eae14d7e8e9e5d5ccce
Samuel Maurice <samuel@acadelib.com>
2021-01-16 22:57:22 +0100

    Ajout des cards

---
+++ src/components/elements/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h5 class="card-title">{{ title }}</h5>
+      <h6 class="card-subtitle">{{ subtitle }}</h6>
+    </div>
+
+    <div class="card-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/elements/card";
+</style>
